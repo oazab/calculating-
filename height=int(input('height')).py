259f222af247bcb4b4297d1a6a8298a76d0ce4cd
oazab <120120731+oazab@users.noreply.github.com>
2023-06-08 12:18:44 +0300

    Add files via upload

---
+++ height=int(input('height')).py
@@ -0,0 +1,5 @@
+height=int(input("enter the height of the plane:"))
+speed=int(input("enter the speed of the plane:"))
+time=(height/speed)
+time2=(30*10)/60 # time with minute
+print("time with minutes is",time2)
